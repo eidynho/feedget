e6f77eb65fbf1966caa2f1eff281f2c959b133de
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-05-04 21:43:07 -0300

    adding server + prisma + nodemailer

---
+++ server/src/routes.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma'
+
+export const routes = express.Router()
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "aa67b98c7c51e1",
+    pass: "ff935fe80380a3"
+  }
+});
+
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <abacate@email.com>',
+    to: 'Vinicius Eidy <zviniciuseidy2@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
+  })
+
+  return res.status(201).json({ data: feedback })
+})
